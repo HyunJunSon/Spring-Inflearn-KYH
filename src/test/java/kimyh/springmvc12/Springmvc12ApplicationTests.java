fac34666a1b29a9da8f96dd41941c2f4a5ecaf8e
HyunJunSon <jjang3a@gmail.com>
2024-11-25 10:57:16 +0900

    spring mvc1-2 start

---
+++ src/test/java/kimyh/springmvc12/Springmvc12ApplicationTests.java
@@ -0,0 +1,13 @@
+package kimyh.springmvc12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
