38bea45d77248d88928c483530463af41cb6aa58
HyunJunSon <jjang3a@gmail.com>
2024-12-23 23:39:29 +0900

    spring mvc-db1 transaction#1

---
+++ src/main/java/kimyh/jdbc/service/MemberService2.java
@@ -0,0 +1,65 @@
+package kimyh.jdbc.service;
+
+import kimyh.jdbc.domain.Member;
+import kimyh.jdbc.repository.MemberRepositoryV1;
+import kimyh.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberService2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        Connection con = dataSource.getConnection();
+        try{
+            con.setAutoCommit(false); // 트랙잭션 시작
+            //비지니스 로직
+            bizLogic(con, fromId, toId, money);
+            con.commit();
+        }catch (Exception e){
+            con.rollback();
+            throw new IllegalStateException(e);
+        }finally {
+            release(con);
+        }
+
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e.getMessage(), e);
+                throw new IllegalStateException(e);
+            }
+        }
+
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(con, toId, toMember.getMoney() + money);
+
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
---
+++ src/test/java/kimyh/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,74 @@
+package kimyh.jdbc.service;
+
+
+import kimyh.jdbc.domain.Member;
+import kimyh.jdbc.repository.MemberRepositoryV1;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+
+import static kimyh.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * 기본동작, 트랙잭셔니 없어서 문제 발생
+ */
+public class MemberServiceV1Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberService1 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberService1(memberRepository);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        memberRepository.save(new Member(MEMBER_A, 10000));
+        memberRepository.save(new Member(MEMBER_B, 10000));
+        // when
+        memberService.accountTransfer(MEMBER_A, MEMBER_B, 2000);
+        // then
+        assertThat(memberRepository.findById(MEMBER_A).getMoney()).isEqualTo(8000);
+        assertThat(memberRepository.findById(MEMBER_B).getMoney()).isEqualTo(12000);
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    void accountTransferEx() throws SQLException {
+
+        Member member_A = memberRepository.save(new Member(MEMBER_A, 10000));
+        Member member_EX = memberRepository.save(new Member(MEMBER_EX, 10000));
+
+        assertThatThrownBy(() -> memberService.accountTransfer(MEMBER_A, MEMBER_EX, 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+
+        Member findMember_A = memberRepository.findById(member_A.getMemberId());
+        Member findMember_EX = memberRepository.findById(member_EX.getMemberId());
+        assertThat(findMember_A.getMoney()).isEqualTo(8000);
+        assertThat(findMember_EX.getMoney()).isEqualTo(10000); // A의 돈만 줄어 들었다.
+
+    }
+}
